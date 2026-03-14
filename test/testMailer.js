e8d2c73f6d9d5a95aa34b4d91694c97f7d15f26b
Phang Jan Yip <168205352+qwuack@users.noreply.github.com>
2026-03-14 17:58:22 +0800

    Delete test directory

--- test/testMailer.js
+++
@@ -1,8 +0,0 @@
-// node test/testMailer.js
-
-require("../config"); // this loads env.local
-const sendResetEmail = require('../mailer');
-
-sendResetEmail(process.env.SMTP_EMAIL, process.env.SMTP_PASSWORD)
-    .then(() => console.log("Email sent"))
-    .catch(console.error);
